dfd74d18d54fb84aa685436e657867aa6ccbd2c7
adelinesharla <adelinesharla@duck.com>
2024-09-28 19:24:27 -0300

    fix: references in output

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "aurora_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = var.vpc_cidr_blocks
+    cidr_blocks = var.vpc_cidr_block
   }
 
   egress {
